50228e5258fefbe358d00040ef9ca5ac7b67353a
prudence <prudeahimbisibwe@gmail.com>
2022-10-25 00:12:44 +0300

    Created Navbar

---
+++ src/Componets/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar() {
+return (
+<div className=" nav">
+<title>Future of Fashion</title>
+     <nav class="navbar background">
+         <ul class="nav-list">
+           <li><a href="#home">Home</a></li>
+           <li><a href="#about">About</a></li>
+           <li><a href="#category">Cotegories</a></li>
+           <li><a href='#contact'>Contact</a></li>
+          </ul>
+      </nav>
+ </div>    
+     )
+}      
+  export default Navbar;
